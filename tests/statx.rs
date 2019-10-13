42218f975045a30cdd55e18690ad861ef8621b57
oxalica <oxalicc@pm.me>
2019-10-13 16:57:52 +0800

    Test statx on root and allow failure of ENOSYS

--- tests/statx.rs
+++ tests/statx.rs
@@ -1,24 +1,36 @@
 extern crate statx_sys;
-use libc::c_char;
+use libc::{c_char, ENOSYS};
 use statx_sys::*;
+use std::{io, mem};
 
-#[test]
-fn test_statx() {
-    use libc::c_int;
-    use std::{io, mem};
-
-    const AT_FDCWD: c_int = -100; // Not contained in low version of libc
-
-    // Working dir
-    let c_path = b".\0";
+fn run_statx(allow_nosys: bool) {
+    // Test on root
+    let c_path = b"/\0";
     let c_path = c_path as *const _ as *mut c_char;
 
     let mut buf = unsafe { mem::zeroed() };
     let ret = unsafe { statx(AT_FDCWD, c_path, 0, STATX_ALL, &mut buf) };
-    if ret != 0 {
-        let err = io::Error::last_os_error();
-        panic!("statx() failed: {:?}", err);
-    } else {
+    if ret == 0 {
         println!("statx() success: {:?}", buf);
+    } else {
+        let err = io::Error::last_os_error();
+        if allow_nosys && err.raw_os_error().unwrap() == ENOSYS {
+            eprintln!("warning: statx() is not supported");
+        } else {
+            panic!("statx() failed: {:?}", err);
+        }
     }
+}
+
+#[test]
+fn test_stat_invoke() {
+    run_statx(true);
+}
+
+/// Syscall `statx` has version requirement of Linux kernel,
+/// so it is not always available.
+#[test]
+#[ignore]
+fn test_stat_success() {
+    run_statx(false);
 }
